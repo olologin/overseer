829827ae3a3a08294f1cb1c47ccb7cab515bf4b6
Alexey Tokar <tokar.a.v@yandex.ru>
2015-10-12 01:43:53 +0500

    Renamed whole project to match domain

--- web/src/main/java/ru/hdghg/spy/web/servlet/SpyServlet.java
+++ web/src/main/java/ru/hdghg/spy/web/servlet/SpyServlet.java
@@ -1,9 +1,9 @@
-package wildfly.wildfly.web.servlet;
+package ru.hdghg.spy.web.servlet;
 
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.core.util.StatusPrinter;
 import org.slf4j.LoggerFactory;
-import wildfly.wildfly.ejb.SpyBean;
+
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
@@ -13,8 +13,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
 import org.slf4j.Logger;
+import ru.hdghg.spy.ejb.SpyBean;
 
 @WebServlet(urlPatterns = {"/spy"})
 public class SpyServlet extends HttpServlet {
@@ -39,7 +41,7 @@ public class SpyServlet extends HttpServlet {
             out.println("<title>Singleton Bean</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Singleton Bean</h1>");
+            out.println("<h1>Date is:" + new Date() + "</h1>");
             out.println("<h2>Container-managed Concurrency</h2>");
             out.println("Spybean start " + spyBean.start() + "<br />");
             out.println("Spybean stop " + spyBean.stop() + "<br />");
