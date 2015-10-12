08eba6ee8d757f0f0147f026be7ab9c2a6625979
Alexey Tokar <tokar.a.v@yandex.ru>
2015-10-12 12:32:07 +0500

    Ensure openshift supports java level 8

--- ejb/src/main/java/ru/hdghg/spy/ejb/SpyBean.java
+++ ejb/src/main/java/ru/hdghg/spy/ejb/SpyBean.java
@@ -24,7 +24,7 @@ public class SpyBean {
     @Lock(LockType.WRITE)
     public boolean stop() {
         log.info("Stop called");
-        return false;
+        return true;
     }
 
 }
--- web/src/main/java/ru/hdghg/spy/web/servlet/SpyServlet.java
+++ web/src/main/java/ru/hdghg/spy/web/servlet/SpyServlet.java
@@ -1,10 +1,5 @@
 package ru.hdghg.spy.web.servlet;
 
-import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.core.util.StatusPrinter;
-import org.slf4j.LoggerFactory;
-
-
 import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
+import java.util.function.BooleanSupplier;
 
 import org.slf4j.Logger;
 import ru.hdghg.spy.ejb.SpyBean;
@@ -30,20 +26,20 @@ public class SpyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.info("Page called");
-        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
-        StatusPrinter.print(lc);
+
+        String acn = request.getParameter("action");
+        BooleanSupplier b = "start".equals(acn) ? spyBean::start : "stop".equals(acn) ? spyBean::stop : () -> false;
 
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Singleton Bean</title>");
+            out.println("<title>Spy control panel</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Date is:" + new Date() + "</h1>");
-            out.println("Spybean start " + spyBean.start() + "<br />");
-            out.println("Spybean stop " + spyBean.stop() + "<br />");
+            out.println("Action run: " + b.getAsBoolean() + "<br />");
             out.println("</body>");
             out.println("</html>");
         }
