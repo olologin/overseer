b6f6c91dc9fc316b7c67c0fab0ff8470fbc1d4eb
tokar-a-v <tokar.a.v@yandex.ru>
2015-10-31 15:04:43 +0500

    SSMW: and atlast we can try our migration engine! (fingers crossed)

--- database/ssmw/run_local.bat
+++ database/ssmw/run_local.bat
@@ -5,4 +5,4 @@ set PGLOGIN=wildfly
 set PGPASSWORD=wildfly
 for /f %%i in ('C:\PROGRA~1\POSTGR~1\9.2\bin\psql.exe -c "copy (select 1) to stdout;" -q -U wildfly') do set GIVEN_VER=%%i
 
-for /f %%i in ('C:\Programs\Python26\python.exe ssmw.py %CREATION% %MIGRATION% %GIVEN_VER% %REQUIRED_VER%') do C:\PROGRA~1\POSTGR~1\9.2\bin\psql.exe -q -U %PGLOGIN% -f %%i
+for /f %%i in ('C:\Programs\Python26\python.exe ssmw.py %CREATION% %MIGRATION% %GIVEN_VER% %REQUIRED_VER%') do C:\PROGRA~1\POSTGR~1\9.2\bin\psql.exe -q -a -U %PGLOGIN% -f %%i
