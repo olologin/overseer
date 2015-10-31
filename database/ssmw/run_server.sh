99a4e7aae1ba371790ef8bc33f80d6d5201a385a
tokar-a-v <tokar.a.v@yandex.ru>
2015-10-31 15:24:52 +0500

    SSMW: Last step. Version gathering is correct now

--- database/ssmw/run_server.sh
+++ database/ssmw/run_server.sh
@@ -3,6 +3,6 @@ export CREATION=$OPENSHIFT_REPO_DIR/database/create/
 export MIGRATION=$OPENSHIFT_REPO_DIR/database/migrate/
 export REQUIRED_VER="$(cat $OPENSHIFT_REPO_DIR/database/version.properties)"
 export PGLOGIN=$OPENSHIFT_POSTGRESQL_DB_USERNAME
-export GIVEN_VER="$(psql -c 'copy (select 1) to stdout;' -q -U $PGLOGIN)"
+export GIVEN_VER="$(psql -c 'copy (select id from version) to stdout;' -q -U $PGLOGIN)"
 
 python $OPENSHIFT_REPO_DIR/database/ssmw/ssmw.py $CREATION $MIGRATION $GIVEN_VER $REQUIRED_VER | while read line ; do psql -q -a -U $PGLOGIN -f $line ; done 
