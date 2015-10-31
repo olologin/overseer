2b013008658d91e763d719e0d8614df1ef8ed171
tokar-a-v <tokar.a.v@yandex.ru>
2015-10-31 15:20:34 +0500

    SSMW: Migrations debug

--- database/migrate/2015-10/5to6.sql
+++ database/migrate/2015-10/5to6.sql
@@ -1,21 +1,7 @@
 -- Migration from state 5 to state 6
 -- Author atokar
-create table
-    version(
-        id bigint not null
-    )
-;
-
-delete from
-    version
-;
-
-insert into
-    version(
-        id
-    )
-values
-    6
-;
+create table if not exists version(id bigint not null);
+delete from version;
+insert into version(id) values (6);
     
 
